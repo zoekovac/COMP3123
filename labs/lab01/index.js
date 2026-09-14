f4d959890e05ac2db9fe080687cbb3c28655d0a6
Zoë Kovac <230038938+zoekovac@users.noreply.github.com>
2026-09-13 21:24:08 -0400

    feat: add JS function to move last three chars to the start of a string

--- labs/lab01/index.js
+++ labs/lab01/index.js
@@ -30,3 +30,21 @@ console.log("\n==== EXERCISE 2 ====");
 console.log(max(1, 0, 1));
 console.log(max(0, -10, -20));
 console.log(max(1000, 510, 440));
+
+// =======================================================================
+// EXERCISE 3 – Move the Last Three Characters to the Start of a String
+// =======================================================================
+function right(str) {
+
+    // Return the string unchanged if its length is less than three
+    if (str.length < 3) {
+        return str;
+    }
+
+    return str.slice(-3) + str.slice(0, -3);
+}
+
+console.log("\n==== EXERCISE 3 ====");
+console.log(right("Python"));
+console.log(right("JavaScript"));
+console.log(right("Hi"));
